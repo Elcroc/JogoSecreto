079449c35b85317f4e4c7b894922e5b4654b2ede
Elcroc <jesiel-mamedes@hotmail.com>
2025-08-11 12:58:18 -0400

    numero 10 estava no lugar de 100

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function verificarChute() {
 }
 function mensagemInicial (){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * 100 + 1);
